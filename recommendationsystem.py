c494a012b9175e8a166952b1e0d4b4a54ef83c71
Dhruv Ajay Toshniwal <57616258+DhruvAjayToshniwal@users.noreply.github.com>
2023-04-30 17:42:03 -0400

    Update recommendationsystem.py

--- recommendationsystem.py
+++ recommendationsystem.py
@@ -18,21 +18,27 @@ def process_chunk(chunk, user, recommendations):
 
             for video, score in sim_scores:
                 title = chunk.iloc[video]["Title"]
-                engagement_score = (chunk.iloc[video]["UserTime"] * chunk.iloc[video]["Likes"] * chunk.iloc[video]["Shares"]) / (chunk.iloc[video]["Dislikes"] + 1)
+                engagement_score = (chunk.iloc[video]["UserTime"] * (chunk.iloc[video]["Likes"] + chunk.iloc[video]["Shares"] - chunk.iloc[video]["Dislikes"])) + chunk.iloc[video]["Comments"] + chunk.iloc[video]["ViewCount"]
                 recommendations[title] += engagement_score * chunk.iloc[video]["NumberOfTimesWatched"]
 
 users = ["Dhruv", "Ron", "Inbar", "Daniel", "Mor", "Guy", "Memi", "Giulia"]
 
-for user in users:
-    print(f"Top 3 recommendations for {user}:")
-    recommendations = defaultdict(int)
+# Create a dictionary to store the total engagement score for each user for each day
+user_engagement = defaultdict(list)
 
-    for i in range(1, 6):
+for i in range(1, 6):
+    print(f"\nDay {i} recommendations:")
+    for user in users:
+        print(f"\nTop 3 recommendations for {user}:")
+        recommendations = defaultdict(int)
+
         chunk = pd.read_csv(f"dataset_{i}.csv")
         process_chunk(chunk, user, recommendations)
 
-    sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
-    top_recommendations = [video[0] for video in sorted_recommendations[:3]]
+        sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+        top_recommendations = [video[0] for video in sorted_recommendations[:3]]
+        
+        print(top_recommendations)
 
-    print(top_recommendations)
-    print()
+        # add the total engagement score of the top 3 recommendations for the user for the day
+        user_engagement[user].append(sum([recommendations[video] for video in top_recommendations]))
