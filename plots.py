be1775e0d06cb7d1be03b69918ecbf1c12010989
Dhruv Ajay Toshniwal <57616258+DhruvAjayToshniwal@users.noreply.github.com>
2023-04-30 17:42:33 -0400

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,9 +1,11 @@
-# Video vs video engagement graph
-video_engagement = chunk.groupby("Title")[["Likes", "Shares", "Comments", "Dislikes"]].sum()
-video_engagement["Total Engagement"] = video_engagement["Likes"] + video_engagement["Shares"] + video_engagement["Comments"] - video_engagement["Dislikes"]
-video_engagement[["Likes", "Shares", "Comments", "Dislikes"]].plot.bar(stacked=True, figsize=(15, 7), colormap='Pastel1', title="Video vs Video Engagement")
-plt.xlabel("Video")
-plt.ylabel("Engagement")
+# Plot the total engagement score of the recommendations for each user for each day
+for user, engagement in user_engagement.items():
+    plt.plot(range(1, 6), engagement, marker='o', label=user)
+
+plt.xlabel('Day')
+plt.ylabel('Total Engagement Score of Recommendations')
+plt.title('Total Engagement Score of Recommendations for each User over Days')
+plt.legend()
 plt.show()
 
 import pandas as pd
@@ -13,30 +15,43 @@ from collections import defaultdict
 # read in the dataset
 df = pd.concat([pd.read_csv(f"dataset_{i}.csv") for i in range(1, 6)])
 
-# create a dictionary of watched counts by user
-watched_counts = defaultdict(int)
-for _, row in df.iterrows():
-    watched_counts[row['User']] += row['NumberOfTimesWatched']
+# create a dictionary of engagement time by user
+engagement_time = defaultdict(int)
 
-# create the line plot
-users = list(watched_counts.keys())
-watched_times = list(watched_counts.values())
+users = ["Dhruv", "Ron", "Inbar", "Daniel", "Mor", "Guy", "Memi", "Giulia"]
+recommended_videos = {}
 
-plt.plot(users, watched_times)
+for user in users:
+    recommendations = defaultdict(int)
+    user_df = df[df['User'] == user]
+    
+    # Get recommended videos first
+    for idx, row in user_df.iterrows():
+        recommendations[row['Title']] += (row["UserTime"] * (row["Likes"] + row["Shares"] - row["Dislikes"])) + row["Comments"] + row["ViewCount"]
+    
+    sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+    top_recommendations = [video[0] for video in sorted_recommendations[:3]]
+    
+    # add the recommended videos to the dict
+    recommended_videos[user] = top_recommendations
+
+    # print the recommended videos
+    print(f"Recommended videos for {user}: {top_recommendations}")
+
+# calculate engagement time for recommended videos
+for user in users:
+    user_df = df[df['User'] == user]
+    for idx, row in user_df.iterrows():
+        if row['Title'] in recommended_videos[user]:
+            engagement_time[user] += row['UserTime'] * row['NumberOfTimesWatched']
+
+# create the bar plot
+users = list(engagement_time.keys())
+total_engagement_times = list(engagement_time.values())
+
+plt.bar(users, total_engagement_times)
 plt.xlabel('User')
-plt.ylabel('Number of times video watched')
-plt.title('User vs Number of times video watched')
-
-# mark recommended videos for Giulia
-giulia_recommended = ['Condom Challenge by Nelk Boys', 'Condom Challenge by Pewdiepie', 'Condom Challenge by Joe Rogan']
-if 'Giulia' in users:
-    giulia_index = users.index('Giulia')
-    giulia_watched_times = watched_times[giulia_index]
-    plt.plot(giulia_index, giulia_watched_times, 'ro')
-    for i, video in enumerate(top_recommendations):
-        if video in giulia_recommended:
-            # get the video title from the dataset
-            video_title = df.loc[df['Title'] == video, 'Title'].values[0]
-            plt.annotate(f'Recommended {i+1}: {video_title}', xy=(giulia_index, giulia_watched_times), xytext=(giulia_index+0.1, giulia_watched_times+50), arrowprops=dict(facecolor='black', shrink=0.05))
+plt.ylabel('Total Engagement Time')
+plt.title('User vs Total Engagement Time for Recommended Videos')
 
 plt.show()
